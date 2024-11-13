bcb53b4a2d138d06525d2460ffbe949587cdf306
Luan Teixeira <luan.teixeira@izatta.tech>
2024-11-13 20:22:54 -0300

    Initit spring boot service

---
+++ anafit-service/src/main/java/luanjesus/tech/anafit_service/AnaFitServiceApplication.java
@@ -0,0 +1,13 @@
+package luanjesus.tech.anafit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaFitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnaFitServiceApplication.class, args);
+	}
+
+}
---
+++ anafit-service/src/test/java/luanjesus/tech/anafit_service/AnaFitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package luanjesus.tech.anafit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaFitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
